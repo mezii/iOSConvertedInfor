43826231577ab5f1dfbe8740f4cbb9b7462c517e
mezii <deptrai.team.pro@gmail.com>
2021-08-03 23:23:39 +0700

    zz

--- index.js
+++ index.js
@@ -476,13 +476,11 @@ const deviceInfoUrl = `http://139.180.128.184:9999/api/fakeinfo/`;
 const deviceInfoOldUrl = `http://139.180.128.184:9999/api/fakeinfo/oldDevice`
 
 app.get("/device/new", async (req, res) => {
-  let ip = req.query.ip;
+  var ip = req.query.ip;
 
   if (req.query.ip = null){
-    ip = req.headers['x-forwarded-for'] ||
-    req.connection.remoteAddress ||
-    req.socket.remoteAddress ||
-    (req.connection.socket ? req.connection.socket.remoteAddress : null);
+    ip = request.connection.remoteAddress;
+
   } 
   const os = req.query.os;
   const device = req.query.device;
