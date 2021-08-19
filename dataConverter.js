b01e074f67233255b55b2d7d6c2c5057cf98857d
mezii <deptrai.team.pro@gmail.com>
2021-08-19 08:22:49 +0700

    zz

--- dataConverter.js
+++ dataConverter.js
@@ -1,10 +1,67 @@
 const fs = require("fs");
 const {iphone} = require("./iphone")
+var randomip = require('random-ip');
+
 const data =  fs.readFileSync("./data.txt", {
   encoding: "utf8",
   flag: "r",
 });
 
+
+const randomLocalRouterIps = [
+	"10.0.0.1",
+	"10.0.0.138",
+	"10.0.0.2",
+	"10.0.1.1",
+	"10.1.1.1",
+	"10.1.10.1",
+	"10.10.1.1",
+	"10.90.90.90",
+	"192.168.0.1",
+	"192.168.0.10",
+	"192.168.0.100",
+	"192.168.0.101",
+	"192.168.0.227",
+	"192.168.0.254",
+	"192.168.0.3",
+	"192.168.0.30",
+	"192.168.0.50",
+	"192.168.1.1",
+	"192.168.1.10",
+	"192.168.1.100",
+	"192.168.1.200",
+	"192.168.1.210",
+	"192.168.1.220",
+	"192.168.1.254",
+	"192.168.1.99",
+	"192.168.10.1",
+	"192.168.10.10",
+	"192.168.10.100",
+	"192.168.10.50",
+	"192.168.100.1",
+	"192.168.100.100",
+	"192.168.102.1",
+	"192.168.11.1",
+	"192.168.123.254",
+	"192.168.15.1",
+	"192.168.16.1",
+	"192.168.168.168",
+	"192.168.178.1",
+	"192.168.2.1",
+	"192.168.2.254",
+	"192.168.20.1",
+	"192.168.223.100",
+	"192.168.251.1",
+	"192.168.254.254",
+	"192.168.3.1",
+	"192.168.30.1",
+	"192.168.4.1",
+	"192.168.50.1",
+	"192.168.55.1",
+	"192.168.62.1",
+	"192.168.8.1",
+	"200.200.200.5"
+]
 let llData = async function getllData() {
 
   const dataArr = data.split("\n");
@@ -109,6 +166,11 @@ let llDataForReplacement = async function getllDataForReplacement(json) {
   // Add ReplacementImageList 
   json["TargetApplicationFakeDeviceToken"] = ["com.apple.test"];
   json["TargetApplicationFakeDeviceToken"] = ["com.apple.test"];
+  json["IPV4LAN"] =  randomip(randomLocalRouterIps[Math.floor(Math.random() * randomLocalRouterIps.length)],24);
+  json["IPV6LAN"] = randomip('2001::4702:d9ff:feaf:6d48');
+  json["IPV4CELL"] = randomip('132.12.34.12');
+  json["IPV6CELL"] = randomip('2001::4702:d9ff:feaf:6d48');
+
 
   return json;
 };
